0a457f3d878b24819349b4cea27dc9d343cbe370
RosalynLP <rosalynlaurap@gmail.com>
2019-08-01 11:00:02 +0100

    plotting both global and DIS

--- src/observables/observables.py
+++ src/observables/observables.py
@@ -35,41 +35,63 @@ def matrix_plot_labels(df):
         ticklocs[i] = 0.5 * (startlocs[i + 1] + startlocs[i])
     return ticklocs, ticklabels, startlocs
 
+def plot_observable_ratio(label, fp1_table, fp2_table):
 
-fp1_table = pd.read_table(
-    "./fp1/output/tables/experiment_result_table.csv", dtype={"user_id": float}
+    T_fp1 = fp1_table["theory_central"]
+    T_fp2 = fp2_table["theory_central"]
+
+    # Calculating errors on T_fp1 by taking standard deviation
+    T_fp1_reps = fp1_table.loc[:, fp1_table.columns.str.contains("rep")]
+    T_fp1_errs = T_fp1_reps.std(axis=1)
+
+    # Plotting
+    fig, ax = plt.subplots(figsize=(15, 6))
+    ax.errorbar(range(len(T_fp1)),
+                T_fp1 / T_fp2,
+                yerr=T_fp1_errs / T_fp2,
+                marker=".",
+                ls = '')
+    ticklocs, ticklabels, startlocs = matrix_plot_labels(fp1_table)
+    plt.xticks(ticklocs, ticklabels, rotation=45, fontsize=15)
+    # Shift startlocs elements 0.5 to left so lines are between indexes
+    startlocs_lines = [x - 0.5 for x in startlocs]
+    ymin, ymax = ax.get_ylim()
+    xmin, xmax = ax.get_xlim()
+    ax.hlines(1, xmin, xmax, linestyles="-")
+    ax.vlines(startlocs_lines, ymin, ymax, linestyles="dashed")
+    ax.margins(x=0, y=0)
+    ax.set_title(r"$T_i^d[f_d] / \langle T_i^d[f_p] \rangle$", fontsize=20)
+    plt.savefig(f"../../plots/observables/observable_ratio_{label}.png")
+    return fig
+
+
+# Loading DIS and global experiment tables
+
+fp1_table_DIS = pd.read_table(
+    "./fp1/output/tables/experiment_result_table.csv",
+    dtype={"user_id": float}
 )
 
-fp2_table = pd.read_table(
-    "./fp2/output/tables/experiment_result_table.csv", dtype={"user_id": float}
+fp2_table_DIS = pd.read_table(
+    "./fp2/output/tables/experiment_result_table.csv",
+    dtype={"user_id": float}
 )
 
-# Cutting to same size for comparison
-#fp1_table = fp1_table.query('dataset == "BCDMSD"')
-#fp2_table = fp2_table.query('dataset == "BCDMSD"')
+#fp1_table_global = pd.read_table(
+#    "./fp1_global/output/tables/experiment_result_table.csv",
+#    dtype={"user_id": float}
+#)
 
-T_fp1 = fp1_table["theory_central"]
-T_fp2 = fp2_table["theory_central"]
-
-# Calculating errors on T_fp1 by taking standard deviation
-T_fp1_reps = fp1_table.loc[:, fp1_table.columns.str.contains("rep")]
-T_fp1_errs = T_fp1_reps.std(axis=1)
+fp2_table_global = pd.read_table(
+    "./fp2_global/output/tables/experiment_result_table.csv",
+    dtype={"user_id": float}
+)
 
 # Plotting
-fig, ax = plt.subplots(figsize=(15, 6))
-ax.errorbar(range(len(T_fp1)),
-            T_fp1 / T_fp2,
-            yerr=T_fp1_errs / T_fp2,
-            marker=".",
-            ls = '')
-ticklocs, ticklabels, startlocs = matrix_plot_labels(fp1_table)
-plt.xticks(ticklocs, ticklabels, rotation=45, fontsize=15)
-# Shift startlocs elements 0.5 to left so lines are between indexes
-startlocs_lines = [x - 0.5 for x in startlocs]
-ymin, ymax = ax.get_ylim()
-xmin, xmax = ax.get_xlim()
-ax.hlines(1, xmin, xmax, linestyles="-")
-ax.vlines(startlocs_lines, ymin, ymax, linestyles="dashed")
-ax.margins(x=0, y=0)
-ax.set_title(r"$T_i^d[f_d] / \langle T_i^d[f_p] \rangle$", fontsize=20)
-plt.savefig("../../plots/observables/observable_ratio.png")
+
+plot_observable_ratio("DIS", fp1_table_DIS, fp2_table_DIS)
+
+plot_observable_ratio("global_proton", fp1_table_DIS, fp2_table_global)
+
+
+
