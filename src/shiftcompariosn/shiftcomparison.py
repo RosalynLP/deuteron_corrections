20afb16acb6bd4dec2f7832e5d219e435f7b271f
RosalynLP <rosalynlaurap@gmail.com>
2020-10-07 17:43:26 +0100

    adding error to MMHT

--- src/shiftcompariosn/shiftcomparison.py
+++ src/shiftcompariosn/shiftcomparison.py
@@ -12,7 +12,7 @@ import sys
 
 np.set_printoptions(threshold=sys.maxsize)
 
-sns.set_style(style="white")
+sns.set_style(style="ticks")
 
 import apfel
 
@@ -33,64 +33,104 @@ eps = 1e-10
 Q0 = 2**(1/2) - eps
 Q =  10**(1/2)
 
-apfel.ComputeStructureFunctionsAPFEL(Q0,Q)
 
 F2s_proton = []
 for x in xlha:
-    F2s_proton.append(apfel.F2total(x))
+    F2reps = []
+    for i in range(1,101):
+        apfel.SetReplica(i)
+        apfel.ComputeStructureFunctionsAPFEL(Q0,Q)
+        F2reps.append(apfel.F2total(x))
+    F2s_proton.append(F2reps)
     
+F2s_proton = np.array(F2s_proton)    
+errs_proton = np.std(F2s_proton, axis=1)
+cvs_proton = np.mean(F2s_proton, axis=1)
+
 # Setting deuteron only PDF
 apfel.SetPDFSet("NNPDF31_nnlo_as_0118_deuteron_only")
 apfel.InitializeAPFEL_DIS()
-apfel.ComputeStructureFunctionsAPFEL(Q0,Q)
 
 F2s_deuteron = []
 for x in xlha:
-    F2s_deuteron.append(apfel.F2total(x))
-    
+    F2reps = []
+    for i in range(1,101):
+        apfel.SetReplica(i)
+        apfel.ComputeStructureFunctionsAPFEL(Q0,Q)
+        F2reps.append(apfel.F2total(x))
+    F2s_deuteron.append(F2reps)
+        
+F2s_deuteron = np.array(F2s_deuteron)    
+errs_deuteron = np.std(F2s_deuteron, axis=1)
+cvs_deuteron = np.mean(F2s_deuteron, axis=1)
+
 # Nuclear fit deuteron PDF
 
 apfel.SetPDFSet("nNNPDF20_nlo_as_0118_D2")
 apfel.InitializeAPFEL_DIS()
-apfel.ComputeStructureFunctionsAPFEL(Q0,Q)
 
 F2s_nuc = []
 for x in xlha:
-    F2s_nuc.append(apfel.F2total(x))
+    F2reps = []
+    for i in range(1,251):
+        apfel.SetReplica(i)
+        apfel.ComputeStructureFunctionsAPFEL(Q0,Q)
+        F2reps.append(apfel.F2total(x))
+    F2s_nuc.append(F2reps)
+        
+F2s_nuc = np.array(F2s_nuc)    
+errs_nuc = np.std(F2s_nuc, axis=1)
+cvs_nuc = np.mean(F2s_nuc, axis=1)
     
-cfac_deut = [a/b for (a, b) in zip(F2s_deuteron, F2s_proton)]
-cfac_nuc = [a/b for (a, b) in zip(F2s_nuc, F2s_proton)]
-      
-    
+cfac_deut = cvs_deuteron/cvs_proton
+cfac_nuc = cvs_nuc/cvs_proton
+errs_deut = errs_deuteron/cvs_proton
+errs_nuc = errs_nuc/cvs_proton
+
+
 # MMHT model - generate cfac
-def generate_MMHT_cfac(xvals, N, c1, c2, c3e8, xp):
+def generate_MMHT_cfac(xvals, N, c1, c2, c3e8, xp,
+                       Nerr, c1err,  c2err, c3e8err):
     c1 = np.full_like(xvals, c1)
     c2 = np.full_like(xvals, c2)
     c3 = np.full_like(xvals, c3e8/1e8)
     xp = np.full_like(xvals, xp)
- #   c_3params = (1 + 0.01*N)*(1 + 0.01*c1*(np.log(xp/xvals))**2)
     c_4params = (1 + 0.01*N)*(1 + 0.01*c2*(np.log(xvals/xp))**2
                               + 0.01*c3*(np.log(xvals/xp))**20)
-    return c_4params
+   
+    # Calc error
+    c2err = np.full_like(xvals, c2err)
+    c3err = np.full_like(xvals, c3e8err/1e8)
+    Nerr = np.full_like(xvals, Nerr)
+    var = ((0.01*(1 + 0.01*c2*(np.log(xvals/xp))**2 +
+                 0.01*c3*(np.log(xvals/xp))**20*Nerr))**2
+          + (1 + 0.01*N)**2*((0.01*(np.log(xvals/xp))**2*c2err)**2
+          + (0.01*(np.log(xvals/xp))**20*c3err)**2))
+    err = np.sqrt(var)
 
-c_4params = generate_MMHT_cfac(xlha, 0.589, -0.116, -0.384, 0.0489, 0.03)
-#c_3params = generate_MMHT_cfac(fp1_kintable["x"],-0.490, 0.349, -0.444, 3.40, 0.05)
+    return c_4params, err
+
+c_4params, err = generate_MMHT_cfac(xlha, 0.589, -0.116, -0.384, 0.0489, 0.03,
+                                    0.738, 0.996, 0.182, 0.0056)
 
 
 fig, ax = plt.subplots(figsize=(8,6))
 
-plt.plot(xlha, cfac_deut, label="from fitted deuteron PDF")
-plt.plot(xlha, cfac_nuc, label="from nNNPDF2.0")
-#plt.plot(fp1_kintable["x"], c_3params, "o", label="MMSTWW 3 params")
-plt.plot(xlha, c_4params, "-", label="MMHT2014 NNLO 4 params")
+plt.plot(xlha, cfac_deut, label="from fitted deuteron PDF", color="#756bb1", linewidth="5", linestyle=":")
+plt.plot(xlha, cfac_nuc, label="from nNNPDF2.0", color="#2c7fb8", linestyle="--", linewidth="5")
+plt.fill_between(xlha, cfac_deut-errs_deut, cfac_deut+errs_deut, alpha=0.5, color="#756bb1")
+plt.fill_between(xlha,  cfac_nuc-errs_nuc, cfac_nuc+errs_nuc, alpha=0.5, color="#2c7fb8")
+plt.plot(xlha, c_4params, "-", label="MMHT2014 NNLO 4 params", color="#a1d99b", linestyle="-", linewidth="5")
+plt.fill_between(xlha, c_4params-err, c_4params+err, alpha=0.5, color="#a1d99b")
 ax.set_xlim(0.01,1)
 ax.set_xscale("log")
 #ax.set_ylim([0.94,1.04])
 xmin, xmax = ax.get_xlim()
-ax.set_xlabel("x")
-ax.set_ylabel("Correction factor")
-plt.title(r"$Q^2$ = 10 GeV$^2$")
+ax.set_xlabel("x", fontsize="20")
+ax.set_ylabel("Correction factor", fontsize="20")
+plt.title(r"$Q^2$ = 10 GeV$^2$", fontsize="30")
 ax.hlines(1, xmin, xmax, linestyles="-")
-ax.legend()
+ax.legend(fontsize="15")
+
 
 
