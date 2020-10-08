88ec6ff446b39c1b58c76c30741771521d8a4a99
RosalynLP <rosalynlaurap@gmail.com>
2020-10-08 08:36:56 +0100

    adding more interpolation points and changing scale

--- src/shiftcompariosn/shiftcomparison.py
+++ src/shiftcompariosn/shiftcomparison.py
@@ -16,7 +16,7 @@ sns.set_style(style="ticks")
 
 import apfel
 
-xlha = np.logspace(-2, 0, num=30)[:-2]
+xlha = np.logspace(-2, 0, num=60)[:-2]
 
 # activate some options
 apfel.SetPerturbativeOrder(2)
@@ -31,38 +31,38 @@ apfel.InitializeAPFEL_DIS()
 eps = 1e-10
 
 Q0 = 2**(1/2) - eps
-Q =  10**(1/2)
+Q =  10
 
 
 F2s_proton = []
-for x in xlha:
+for i in range(1,101):
+    apfel.SetReplica(i)
+    apfel.ComputeStructureFunctionsAPFEL(Q0,Q)
     F2reps = []
-    for i in range(1,101):
-        apfel.SetReplica(i)
-        apfel.ComputeStructureFunctionsAPFEL(Q0,Q)
+    for x in xlha:
         F2reps.append(apfel.F2total(x))
     F2s_proton.append(F2reps)
     
 F2s_proton = np.array(F2s_proton)    
-errs_proton = np.std(F2s_proton, axis=1)
-cvs_proton = np.mean(F2s_proton, axis=1)
+errs_proton = np.std(F2s_proton, axis=0)
+cvs_proton = np.mean(F2s_proton, axis=0)
 
 # Setting deuteron only PDF
 apfel.SetPDFSet("NNPDF31_nnlo_as_0118_deuteron_only")
 apfel.InitializeAPFEL_DIS()
 
 F2s_deuteron = []
-for x in xlha:
+for i in range(1,101):
+    apfel.SetReplica(i)
+    apfel.ComputeStructureFunctionsAPFEL(Q0,Q)
     F2reps = []
-    for i in range(1,101):
-        apfel.SetReplica(i)
-        apfel.ComputeStructureFunctionsAPFEL(Q0,Q)
+    for x in xlha:
         F2reps.append(apfel.F2total(x))
     F2s_deuteron.append(F2reps)
         
 F2s_deuteron = np.array(F2s_deuteron)    
-errs_deuteron = np.std(F2s_deuteron, axis=1)
-cvs_deuteron = np.mean(F2s_deuteron, axis=1)
+errs_deuteron = np.std(F2s_deuteron, axis=0)
+cvs_deuteron = np.mean(F2s_deuteron, axis=0)
 
 # Nuclear fit deuteron PDF
 
@@ -70,17 +70,17 @@ apfel.SetPDFSet("nNNPDF20_nlo_as_0118_D2")
 apfel.InitializeAPFEL_DIS()
 
 F2s_nuc = []
-for x in xlha:
+for i in range(1,101):
+    apfel.SetReplica(i)
+    apfel.ComputeStructureFunctionsAPFEL(Q0,Q)
     F2reps = []
-    for i in range(1,251):
-        apfel.SetReplica(i)
-        apfel.ComputeStructureFunctionsAPFEL(Q0,Q)
+    for x in xlha:
         F2reps.append(apfel.F2total(x))
     F2s_nuc.append(F2reps)
         
 F2s_nuc = np.array(F2s_nuc)    
-errs_nuc = np.std(F2s_nuc, axis=1)
-cvs_nuc = np.mean(F2s_nuc, axis=1)
+errs_nuc = np.std(F2s_nuc, axis=0)
+cvs_nuc = np.mean(F2s_nuc, axis=0)
     
 cfac_deut = cvs_deuteron/cvs_proton
 cfac_nuc = cvs_nuc/cvs_proton
@@ -116,19 +116,20 @@ c_4params, err = generate_MMHT_cfac(xlha, 0.589, -0.116, -0.384, 0.0489, 0.03,
 
 fig, ax = plt.subplots(figsize=(8,6))
 
-plt.plot(xlha, cfac_deut, label="from fitted deuteron PDF", color="#756bb1", linewidth="5", linestyle=":")
-plt.plot(xlha, cfac_nuc, label="from nNNPDF2.0", color="#2c7fb8", linestyle="--", linewidth="5")
-plt.fill_between(xlha, cfac_deut-errs_deut, cfac_deut+errs_deut, alpha=0.5, color="#756bb1")
-plt.fill_between(xlha,  cfac_nuc-errs_nuc, cfac_nuc+errs_nuc, alpha=0.5, color="#2c7fb8")
-plt.plot(xlha, c_4params, "-", label="MMHT2014 NNLO 4 params", color="#a1d99b", linestyle="-", linewidth="5")
-plt.fill_between(xlha, c_4params-err, c_4params+err, alpha=0.5, color="#a1d99b")
+plt.plot(xlha, cfac_deut, label="from fitted deuteron PDF", color="C0", linewidth="5", linestyle=":")
+plt.plot(xlha, cfac_nuc, label="from nNNPDF2.0", color="C1", linestyle="--", linewidth="5")
+plt.fill_between(xlha, cfac_deut-errs_deut, cfac_deut+errs_deut, alpha=0.5, color="C0")
+plt.fill_between(xlha,  cfac_nuc-errs_nuc, cfac_nuc+errs_nuc, alpha=0.5, color="C1")
+plt.plot(xlha, c_4params, "-", label="MMHT2014 NNLO 4 params", color="C2", linestyle="-", linewidth="5")
+plt.fill_between(xlha, c_4params-err, c_4params+err, alpha=0.5, color="C2")
 ax.set_xlim(0.01,1)
 ax.set_xscale("log")
-#ax.set_ylim([0.94,1.04])
+ax.set_ylim([0.6,1.2])
 xmin, xmax = ax.get_xlim()
 ax.set_xlabel("x", fontsize="20")
 ax.set_ylabel("Correction factor", fontsize="20")
-plt.title(r"$Q^2$ = 10 GeV$^2$", fontsize="30")
+plt.title(r"$Q$ = 10 GeV", fontsize="30")
+ax.tick_params(labelsize="16")
 ax.hlines(1, xmin, xmax, linestyles="-")
 ax.legend(fontsize="15")
 
